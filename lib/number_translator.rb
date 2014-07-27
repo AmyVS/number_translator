95347c66462faac919873ce487ec0935bc247de6
amyvs <amydecat@yahoo.com>
2014-07-26 17:47:51 -0700

    write spec for teens and solve it

--- lib/number_translator.rb
+++ lib/number_translator.rb
@@ -1,14 +1,22 @@
 def number_translator(number)
 	single_digits = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
 									5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+	teen_digits = {10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 
+								15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
 
 	words = []
 
-		if single_digits.has_key?(number)
-			words << single_digits[number]
-
+		if number < 10
+			if single_digits.has_key?(number)
+				words << single_digits[number]
+			end
+		elsif number >= 10 && number < 20
+			if teen_digits.has_key?(number)
+				words << teen_digits[number]
+			end
 		end
+
 	words.join("")
 end
 
-print number_translator(1)
+print number_translator(11)
--- spec/number_translator_spec.rb
+++ spec/number_translator_spec.rb
@@ -5,4 +5,7 @@ describe('number_translator') do
 	it('takes an integer and translates it into its respective words') do
 		expect(number_translator(1)).to(eq('one'))
 	end
+	it('takes an integer in between 10 and 19 and translates it into its respective words') do
+		expect(number_translator(11)).to(eq('eleven'))
+	end
 end
