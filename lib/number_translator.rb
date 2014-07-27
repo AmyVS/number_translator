5f52565ffccc6cdc93fee08804148857877bdbd5
amyvs <amydecat@yahoo.com>
2014-07-26 18:22:20 -0700

    add spec for zero and solve it

--- lib/number_translator.rb
+++ lib/number_translator.rb
@@ -1,8 +1,11 @@
 def number_translator(number)
-	single_digits = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
-									5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-	teen_digits = {10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 
-								15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
+	single_digits = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
+									5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+	teen_digits = { 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 
+								15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 
+								19 => "nineteen" }
+	tens_digits = { 1 => "ten", 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty",
+								6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
 
 	words = []
 
@@ -10,12 +13,16 @@ def number_translator(number)
 			if single_digits.has_key?(number)
 				words << single_digits[number]
 			end
-		elsif number >= 10 && number < 20
+		elsif number > 10 && number < 20
 			if teen_digits.has_key?(number)
 				words << teen_digits[number]
 			end
+		elsif number > 19 && number < 100
+			ten,_,ones = number.partition("")
+			# if ones == 0
+			# 	words << single_digits[number]
+			# end
 		end
-
 	words.join("")
 end
 
--- spec/number_translator_spec.rb
+++ spec/number_translator_spec.rb
@@ -2,10 +2,20 @@ require('rspec')
 require('number_translator')
 
 describe('number_translator') do
-	it('takes an integer and translates it into its respective words') do
+	it('takes an integer and translates it into its respective word or words') do
 		expect(number_translator(1)).to(eq('one'))
 	end
-	it('takes an integer in between 10 and 19 and translates it into its respective words') do
+	it('takes an integer in between 11 and 19 and translates it into its respective word') do
 		expect(number_translator(11)).to(eq('eleven'))
 	end
+	it('takes the integer "0" and returns "zero"') do
+		expect(number_translator(0)).to(eq('zero'))
+	end
+	# it('takes an integer with "0" as the last digit and does not return "zero"') do
+	# 	expect(number_translator(20)).to(eq('twenty'))
+	# end
+	# it('takes an integer in between 20 and 99 and translates it into its respective words') do
+	# 	expect(number_translator(21)).to(eq('twenty one'))
+	# end
+
 end
