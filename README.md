f24a15ab15212ce314f6e6c8dae606a3572512ff
amyvs <amydecat@yahoo.com>
2014-07-27 17:52:29 -0700

    accept changes made directly on GitHub

---
+++ README.md
@@ -0,0 +1,6 @@
+# Number Translator
+
+This is one of my first big Ruby projects. Here's what Number Translator does:
+
+It takes any given number, from 0 to 1 trillion, and translates the number into English words. 
+Pretty neat, eh? Hope you like it!
--- lib/number_translator.rb
+++ lib/number_translator.rb
@@ -1,16 +1,16 @@
 def number_translator(input)
 	single_digits = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
-									5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+			  5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
 
 	teen_digits = { 0 => "ten", 1 => "eleven", 2 => "twelve", 3 => "thirteen", 
-									4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 
-									8 => "eighteen", 9 => "nineteen" }
+			4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 
+			8 => "eighteen", 9 => "nineteen" }
 
 	tens_digits = { 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty",
-									6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
+			6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
 
-	nth_digits = 	{ 1 => " thousand ", 2 => " million ", 3 => " billion ", 
-									4 => " trillion " } 
+	nth_digits =  { 1 => " thousand ", 2 => " million ", 3 => " billion ", 
+			4 => " trillion " } 
 
 	# Final words are pushed here:
 	words = []
